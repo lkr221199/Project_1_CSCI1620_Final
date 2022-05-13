ef992ace8207944deb79f9a2bf1395ba6114bd3d
lkr221199 <lreichwaldt@unomaha.edu>
2022-05-13 14:54:39 -0500

    Created pytest test files

---
+++ test_RPS_game.py
@@ -0,0 +1,31 @@
+import pytest
+import RPS_game
+
+
+class TestRPS(pytest.TestCase):
+
+    def increase_score(self):
+        pass
+
+    def chooses(self):
+        pass
+
+    def chooses_random(self):
+        pass
+
+    def user_selected(self):
+        pass
+
+    def __determine_round_winner(self):
+        pass
+
+    def __determine_outcome(self):
+        pass
+
+    def get_game_info(self):
+        pass
+
+
+if __name__ == '__main__':
+    pytest.main()
+
---
+++ test_controller_Project_1.py
@@ -0,0 +1,19 @@
+import pytest
+import controller_Project_1
+
+
+class TestController1(pytest.TestCase):
+
+    def update_game(self):
+        pass
+
+    def update_display(self):
+        pass
+
+    def reset_game(self):
+        pass
+
+
+if __name__ == '__main__':
+    pytest.main()
+
